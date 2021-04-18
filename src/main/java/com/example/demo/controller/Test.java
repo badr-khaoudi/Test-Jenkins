ea3b392ac6de491fc13dc3355bfbb0e3368a5f5b
badr-khaoudi <b.khaoudi@inspheris.com>
2021-04-18 23:05:23 +0000

    test jenkins

--- src/main/java/com/example/demo/controller/Test.java
+++ src/main/java/com/example/demo/controller/Test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
 
-	@GetMapping("/")
+	@GetMapping("/hello")
 	public String test() {
 		return("Hello World!");
 	}
